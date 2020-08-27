3c2fc995ae3580849df93ef4fd00ecf6fae43a90
Sebastian Rendon <rendonsebastian95@gmail.com>
2020-08-27 18:41:57 -0500

    despliegue de angular

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const {dbConnection} = require('./database/cofig');
 // crear el servidor expres
 const app = express();
 
+const path = require('path');
+
 //configurar cors
 app.use(cors());
 
@@ -36,6 +38,10 @@ app.use( '/api/todo', require('./routes/busquedas') );
 
 app.use( '/api/upload', require('./routes/uploads') );
 
+//lo ultimo
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve( __dirname, 'public/index.html'));
+}
 
 app.listen(process.env.PORT, () => {
     console.log('servidor corriendo en el puerto 3000');
